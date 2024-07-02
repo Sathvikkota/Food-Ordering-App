73864f3064007a32534dd0d780654fed859f5567
Sathvikkota <lalitheshsathvik777@gmail.com>
2024-07-02 19:38:00 +0530

    Initialized BackEnd with Sping Initializer

---
+++ Backend/src/test/java/com/sathvik/Backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sathvik.Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
